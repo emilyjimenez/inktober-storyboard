255e9deb0f829380c77ab1caeeabaf73abf09fc9
Emily Wells Jimenez and Kimberly Bordon <emily@>
2017-10-03 15:02:32 -0700

    Change pen function to rest at specific point upon reaching end of page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
   // $('.parallax').parallax();
 
-  var scrollHeight = $(document).height();
-  var windowHeight = $(window).height();
-  var windowWidth = $(window).width();
-  var offset=$("#stickytest").offset();
 
+  var scrollHeight = $(document).height();
+  var windowWidth = ($(window).width());
+  var windowHeight = $(window).height()/1.25;
   $(window).scroll(function(event){
+
     var scrollTop = $(window).scrollTop();
     var scrollAmount = (scrollTop / (scrollHeight - windowHeight));
-    console.log(Math.sin(scrollAmount * (Math.PI)));
-    $("#penimg").css("left", Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth));
-    $("#penimg").css("top", 10 + Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight));
+    // console.log(Math.sin(scrollAmount * (Math.PI)));
+    var moveTop = 10 + Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight);
+    var moveLeft = Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth);
+
+    // $("#penimg").css("top", 10 + Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight));
+    // $("#penimg").css("left", Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth));
+
+    if (moveTop > 980) {
+      $("#penimg").css("top", 980);
+    } else {
+      $("#penimg").css("top", Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight));
+    }
+    if (moveLeft < 150) {
+      $("#penimg").css("left", 150);
+    } else {
+      $("#penimg").css("left", Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth));
+    }
     // $("#stickytest").css("top", (Math.sin((scrollAmount) * 90) * windowWidth));
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $("#penimg").css("top", 750);
+        $("#penimg").css("left", 150);
+      }
   });
 });
