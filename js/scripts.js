5d14549a8d249719a120d8768265164eb42e653e
Emily Wells Jimenez and Kimberly Bordon <emily@>
2017-10-03 13:21:56 -0700

    Remove extraneous text from merge conflict edit.

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,3 @@ $(document).ready(function(){
     // $("#stickytest").css("top", (Math.sin((scrollAmount) * 90) * windowWidth));
   });
 });
-
-// (Math.sin((scrollAmount) * 180) * windowWidth)
-
-
-
-// $(window).scroll(function(event)){
-//   var image = document.querySelector("#stickytest");
-//   var start = 0;
-//   function sine() {
-//     image.style.left = 50 * Math.sin(start) + 10 + "%"
-//     start +=0.05
-//   });
-//   window.setInterval(sine, 1000/30)
-// });
-// });
