91c21fdba72c46a127707184d29735994d6cd235
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-10-04 11:24:45 -0700

    create inkdrop animation for when user reaches the  footer.

--- js/scripts.js
+++ js/scripts.js
@@ -151,6 +151,9 @@ function dailyPrompt() {
         $("#penimg").css("top", 750);
         $("#penimg").css("left", 150);
       }
+      if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
+        $("#footerimg").fadeIn("slow");
+      }
 
     //IMG HOVER TEXT CAPTION
     $('.hover').mouseover(function() {
@@ -161,6 +164,6 @@ function dailyPrompt() {
       $('.text').css("visibility","hidden");
     });
     //END IMAGE HOVER
-    
+
   });
 });
