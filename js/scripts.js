67f2214b55591482714ff49799cdcdd3f152ea31
Kimberly Bordon <kbordon@gmail.com>
2017-10-04 22:51:32 -0700

    Add box style to design.

--- js/scripts.js
+++ js/scripts.js
@@ -122,19 +122,69 @@ function dailyPrompt() {
   dailyPrompt(); //Call dailyPrompy function on current date
 //END part 2
 
+  // START scrolling prompt boxes
+  var prompts = ["swift", "divided", "poison", "underwater", "long", "sword",
+          "shy", "crooked", "screech", "gigantic", "run", "shattered",
+          "teeming", "fierce", "mystery", "fat", "graceful", "filthy",
+          "cloud", "deep", "furious", "trail", "juicy", "blind", "ship",
+          "squeak", "climb", "fall", "united", "found", "mask"];
+
+  var todayDate = new Date();
+  var dateNumber = todayDate.getDate();
+
+  for (var index = 0; index < dateNumber; index++) {
+    if ( index % 2 === 0) {
+      $("#even-boxes").append("<div class='prompt-box'>" +
+                                + (index + 1) + ". " + prompts[index] +
+                              "</div>");
+    } else {
+      $("#odd-boxes").append("<div class='prompt-box'>" +
+                               + (index + 1) + ". " + prompts[index] +
+                              "</div>");
+    }
+  }
+
+  $(window).scroll(function() {
+    $(".prompt-box").each(function() {
+      var boxTopPosition = $(this).offset().top;
+      var boxOuterHeight = $(this).outerHeight();
+      var windowHalfHeight = $(window).height()/2;
+      var windowScrollTop = $(window).scrollTop();
+      if (windowScrollTop > (boxTopPosition+boxOuterHeight-windowHalfHeight)){
+        $(this).animate({
+          opacity: 1,
+        }, 1200, function() {
+        }).addClass('animated pulse');
+      }
+    });
+  });
+
+
   var scrollHeight = $(document).height();
   var windowWidth = ($(window).width());
   var windowHeight = $(window).height()/1.25;
   $(window).scroll(function(event){
-    // $("#penimg").animate({opacity: 1}, 800, function(){});
+    var docHeight = $(document).height();
     var scrollTop = $(window).scrollTop();
     var scrollAmount = (scrollTop / (scrollHeight - windowHeight));
-    // console.log(Math.sin(scrollAmount * (Math.PI)));
+    console.log(scrollAmount);
     var moveTop = 10 + Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight);
     var moveLeft = Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth);
 
-    // $("#penimg").css("top", 10 + Math.round(Math.sin(scrollAmount * (Math.PI)/2) * windowHeight));
-    // $("#penimg").css("left", Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth));
+    if (scrollAmount > 0.05 && scrollAmount < .07) {
+      $("#splat1").fadeIn();
+    }
+    if (scrollAmount > .07) {
+      $("#splat1").fadeOut();
+    }
+
+    if (scrollAmount > 0.30 && scrollAmount < .40) {
+      $("#splat2").fadeIn();
+    }
+    if (scrollAmount > .40) {
+      $("#splat2").fadeOut();
+    }
+
 
     if (moveTop > 980) {
       $("#penimg").css("top", 980);
@@ -146,7 +196,6 @@ function dailyPrompt() {
     } else {
       $("#penimg").css("left", Math.round(Math.sin(scrollAmount * (Math.PI)) * windowWidth));
     }
-    // $("#stickytest").css("top", (Math.sin((scrollAmount) * 90) * windowWidth));
     if($(window).scrollTop() + $(window).height() > $(document).height() - 750) {
         $("#penimg").css("top", 750);
         $("#penimg").css("left", 150);
@@ -169,4 +218,15 @@ function dailyPrompt() {
     $('.text').css("visibility","hidden");
   });
   // END IMAGE HOVER
+
+  $(".prompt-link").click(function(){
+    $("#all-boxes").fadeOut();
+    $("#prompts").fadeIn();
+  })
+
+  $(".prompt-link2").click(function(){
+    $("#prompts").fadeOut();
+    $("#all-boxes").fadeIn();
+  })
+
 });
